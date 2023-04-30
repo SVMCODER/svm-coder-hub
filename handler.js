ac880feb0cf8d3532a4ffbe7bbd12c9ecf85507e
svmcoder <124701282+SVMCODER@users.noreply.github.com>
2023-04-30 20:49:53 +0530

    Update handler.js

--- handler.js
+++ handler.js
@@ -8,11 +8,21 @@ function save() {
            <img class="icon" src="https://cdn0.iconfinder.com/data/icons/file-type-19/512/file__html__web_-512.png" alt="">
            <p style="color:yellow">Name: ${pname.replace(' ','-')}.html</p>
            <p style="color: rgb(0, 195, 255)">Created on: ${Date().replace('GMT+0530 (India Standard Time)','')}</p>
-            <button onclick="localStorage.getItem('code').select();localStorage.getItem('code').setSelectionRange(0,99999);navigator.clipboard.writeText(localStorage.getItem('code'));swal('Copied!','Code is copied to your clipboard.','success')">Copy Code</button>
-<p></p> <textarea>${localStorage.getItem('code')}</textarea>
+           <button class="bx bxs-copy-alt" onclick="localStorage.getItem('code').select();localStorage.getItem('code').setSelectionRange(0,99999);navigator.clipboard.writeText( localStorage.getItem('code') ); swal('Copied!', 'Code is copied to your clipboard.', 'success')">Copy Code</button>
+          <p></p> <textarea>${localStorage.getItem('code')}</textarea>
         </div>
     <br>
     `)
+    localStorage.setItem('log',localStorage.getItem('log').replace('No Projects Found!','')+`
+    <div class="container">
+            <p style="color: red">
+                Event Data
+            </p>
+           <img class="icon" src="https://cdn2.iconfinder.com/data/icons/flaticons-solid/16/history-1-512.png" alt="">
+           <p style="color:yellow">Type: ${pname} Saved</p>
+           <p style="color: rgb(0, 195, 255)">Timing: ${Date().replace('GMT+0530 (India Standard Time)','')}</p>
+        </div>
+    <br>`)
     swal({
         title: "Saved!",
         text: `Your project ${pname} is saved successfully.`,
@@ -28,6 +38,16 @@ function create() {
     text: `Creating your project, please wait!`,
     imageUrl: 'https://cdn.dribbble.com/users/1186261/screenshots/3718681/_______.gif'
 });
+localStorage.setItem('log',localStorage.getItem('log').replace('No Projects Found!','')+`
+    <div class="container">
+            <p style="color: red">
+                Event Data
+            </p>
+           <img class="icon" src="https://cdn2.iconfinder.com/data/icons/flaticons-solid/16/history-1-512.png" alt="">
+           <p style="color:yellow">Type: Project Created</p>
+           <p style="color: rgb(0, 195, 255)">Timing: ${Date().replace('GMT+0530 (India Standard Time)','')}</p>
+        </div>
+    <br>`)
 setTimeout(() => {
   window.location.replace("newproject.html")
 }, 1500);
