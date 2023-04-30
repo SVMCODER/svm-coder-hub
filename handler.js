0765c294f0766a6873a2568350f3a3891430827d
svmcoder <124701282+SVMCODER@users.noreply.github.com>
2023-04-30 08:38:21 +0530

    Update handler.js

--- handler.js
+++ handler.js
@@ -8,8 +8,8 @@ function save() {
            <img class="icon" src="https://cdn0.iconfinder.com/data/icons/file-type-19/512/file__html__web_-512.png" alt="">
            <p style="color:yellow">Name: ${pname.replace(' ','-')}.html</p>
            <p style="color: rgb(0, 195, 255)">Created on: ${Date().replace('GMT+0530 (India Standard Time)','')}</p>
-           <button class="bx bxs-copy-alt" onclick="navigator.clipboard.writeText(localStorage.getItem('code')); swal('Copied!', 'Code is copied to your clipboard.', 'success')">Copy Code</button>
-          <p></p> <textarea>${localStorage.getItem('code')}</textarea>
+           <button class="bx bxs-copy-alt"onclick="localStorage.getItem('code').select();localStorage.getItem('code').setSelectionRange(0,99999);navigator.clipboard.writeText(localStorage.getItem('code'));swal('Copied!','Code is copied to your clipboard.','success')">Copy Code</button>
+           <p></p> <textarea>${localStorage.getItem('code')}</textarea>
         </div>
     <br>
     `)
